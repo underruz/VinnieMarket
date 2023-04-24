3dab51d7de8e1d2aba8110d84834131720129f04
unknown <underruz@gmail.com>
2023-04-24 10:53:41 +1000

    Vinnie_Market

--- main/urls.py
+++ main/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from main import views
+
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+
 ]
